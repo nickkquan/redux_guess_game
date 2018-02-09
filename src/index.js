4d54f0a86c18968fb33a1c1272737fc5a22f6f8a
Nick Quan <nickkquan@gmail.com>
2018-02-08 17:06:16 -0800

    redux setup completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-import App from './components/app';
+import rootReducer from "./reducers";
+import App from "./components/app";
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+	<Provider store={createStore(rootReducer)}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
 );
